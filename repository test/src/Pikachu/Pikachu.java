2682a05f1b6f66387317aecd6eedd52ba65ebf59
Dicoguy <sigore11@yahoo.com>
2017-08-30 10:39:31 -0700

    Pikachu

---
+++ repository test/src/Pikachu/Pikachu.java
@@ -0,0 +1,9 @@
+package Pikachu;
+
+public class Pikachu {
+	public static void main(String[] args) {
+		System.out.println("  (\\__/)");
+		System.out.println("  (o^.^)");
+		System.out.println("z_(\")(\")");
+	}
+}
