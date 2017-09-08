b70caed277b1e66dc6289d4950dbdbac2a3b3013
Dicoguy <sigore11@yahoo.com>
2017-09-08 09:10:27 -0700

    added foil and other comments

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -15,7 +15,7 @@ public class DoMath {
 		System.out.println(Calculate.discriminant(5, 3, 5));
 		System.out.println(Calculate.toImproperFrac(3, 1, 2));
 		System.out.println(Calculate.toMixedNum(5,2));
-		
+		System.out.println(Calculate.foil(2,3,6,-7,"n"));
 		System.out.println(Calculate.absValue(-3));
 	}
 }
