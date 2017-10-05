1593e3015e3eb2ea04eb774e48c27c752a658c27
Dicoguy <sigore11@yahoo.com>
2017-10-05 09:47:31 -0700

    Pair Programming

---
+++ Pair Programming/src/PairProgram.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class PairProgram {
+	public static void main(String args []) {
+		Scanner userInput = new Scanner(System.in);
+		int sum = 0;
+		int min = userInput.nextInt();
+		int max = userInput.nextInt();
+		boolean done = false;
+		while(!done){
+			int num = userInput.nextInt();
+			min = num;
+			if(num < min) {
+				min = num;
+			}
+			if(num > max) {
+				max = num;
+			}
+			if(num % 2 == 0) {
+				sum += num;
+			}
+}
+	
