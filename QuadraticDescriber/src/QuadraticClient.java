fed5a31447b3c0bc6087813ea579d985b26edcce
Dicoguy <sigore11@yahoo.com>
2017-10-10 10:00:42 -0700

    First push of Quadratic and QuadraticDescriber

---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,24 @@
+//Marcus Wong
+//October 10th, 2017
+//2nd Period
+/*This program contains the
+ * scanners that handle user input
+ * for Quadratic.java
+ */
+
+
+import java.util.*;
+
+public class QuadraticClient {
+	public static void main(String args[]) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("a: ");
+		double a = userInput.nextDouble();
+		System.out.println("b: ");
+		double b = userInput.nextDouble();
+		System.out.println("c: ");
+		double c = userInput.nextDouble();
+		
+	}
+	
+}
