9dd1ffdf4afb42113adeda3486ad4dab7f696caa
Dicoguy <sigore11@yahoo.com>
2017-10-10 10:29:06 -0700

    starting edits for quads

--- QuadraticDescriber/src/Quadratic.java
+++ QuadraticDescriber/src/Quadratic.java
@@ -1,4 +1,28 @@
-
+//Marcus Wong
+//October 10th, 2017
+/* This program contains the various functions
+ * needed to describe the quadratic graph of the 
+ * values gained in QuadraticClient
+ */
 public class Quadratic {
-
+	public static String quadratic(double a, double b, double c) {
+		String answer = "";
+		
+		String description = "Description of the Graph is: \n";
+		String equation = "y = " + a + " x^2 + " + b + " x + " + c;
+		answer += description + equation + "\n\n";
+		
+		if(a < 0) {
+			answer += "Opens: Down \n";
+		}
+			answer += "Opens: Up \n";
+		String axis = "Axis of Symmetry:";	
+		double AOS = (-b / (2 * a));
+		answer += axis + AOS + "\n";
+		
+		double vertex = ((a * (AOS * AOS)) + (b * AOS) + c);
+		answer += "<" + AOS + "," + vertex + "> \n";
+		
+		return answer;
+	}
 }
--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -12,12 +12,14 @@ import java.util.*;
 public class QuadraticClient {
 	public static void main(String args[]) {
 		Scanner userInput = new Scanner(System.in);
-		System.out.println("a: ");
+		System.out.println("Provide values for coefficients a, b, and c");
+		System.out.print("a: ");
 		double a = userInput.nextDouble();
-		System.out.println("b: ");
+		System.out.print("b: ");
 		double b = userInput.nextDouble();
-		System.out.println("c: ");
+		System.out.print("c: ");
 		double c = userInput.nextDouble();
+		System.out.println(Quadratic.quadratic(a,b,c));
 		
 	}
 	
