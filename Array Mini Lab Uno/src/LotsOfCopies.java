4b310afae1b441f05d2ea89e16613af1161a0666
Dicoguy <sigore11@yahoo.com>
2017-10-30 09:51:25 -0700

    lots of copies

--- Array Mini Lab Uno/src/LotsOfCopies.java
+++ Array Mini Lab Uno/src/LotsOfCopies.java
@@ -11,11 +11,23 @@ public class LotsOfCopies {
 		poop(num,strMain, arrMain);
 		System.out.println(num + " " + strMain + " " + Arrays.toString(arrMain));
 		
+		int a = 1;
+		int b = a;
+		int c = 4;
+		System.out.println(a);
+		System.out.println(b);
+		poop2(a);
+		
+		
 	}
 	public static void poop(int x, String word, int[] arr) {
 		x++;
 		word += "poop";
 		arr[2] = 42;
 		System.out.println(x + " " + word + " " + arr[2]);
+	}
+	public static void poop2(int num) {
+		num++;
+		System.out.println(num);
 	}
 }
