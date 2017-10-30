7d5c1a7267e79fe6a12ff52e4cd45dca3d790386
Dicoguy <sigore11@yahoo.com>
2017-10-30 09:45:13 -0700

    array lab committ and something about split

---
+++ Array Mini Lab Uno/src/LotsOfCopies.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+//Marcus Wong
+//10/30/17
+public class LotsOfCopies {
+	public static void main(String[] args) {
+		int num = 7;
+		String strMain = "APCS";
+		int[] arrMain = {1, 2, 3, 4 , 5};
+		System.out.println(num + " " + strMain + " " + Arrays.toString(arrMain));
+		poop(num,strMain, arrMain);
+		System.out.println(num + " " + strMain + " " + Arrays.toString(arrMain));
+		
+	}
+	public static void poop(int x, String word, int[] arr) {
+		x++;
+		word += "poop";
+		arr[2] = 42;
+		System.out.println(x + " " + word + " " + arr[2]);
+	}
+}
--- Split/src/split.java
+++ Split/src/split.java
@@ -1,3 +1,6 @@
+//Marcus Wong
+//10/28/17
+//2nd Period
 import java.util.*;
 public class split {
 
@@ -6,9 +9,13 @@ public class split {
 		System.out.println(Arrays.toString("I like apples!".split(" ")));
 		System.out.println(Arrays.toString("I HATE those dumb apples!".split(" ")));
 		System.out.println(Arrays.toString("The cow exclaimed in a suprised tone MEOW".split(" ")));
+		
 		//Part 1
 		String sandwich = "apples bread pineapples bread lettus bread tomato bacon bread mayo ham bread cheese";
 		System.out.println(sandwichMiddle(sandwich));
+		//Part 2
+		String sandwich2 = "apples bread pineapples bread lettus bread tomato bacon bread mayo ham bread cheese";
+		
 	}
 	public static String sandwichMiddle(String sandwich) {
 		String[] toppings = new String[sandwich.split("bread").length];
@@ -19,8 +26,8 @@ public class split {
 			toppings[i] = sandwich.split("bread")[i];
 		}
 		
-		for (int i = 2; i < toppings.length-1; i++) {
-			middle += toppings[i] + " ";
+		for (int i = 1; i < toppings.length-1; i++) {
+			middle += toppings[i];
 		}
 
 		return middle;
