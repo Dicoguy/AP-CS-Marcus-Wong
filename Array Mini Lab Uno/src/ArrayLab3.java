8fab487514d0c0c0d758193394475926a62376aa
Dicoguy <31106989+Dicoguy@users.noreply.github.com>
2017-11-05 18:03:45 -0800

    array lab 3 final

--- Array Mini Lab Uno/src/ArrayLab3.java
+++ Array Mini Lab Uno/src/ArrayLab3.java
@@ -1,3 +1,6 @@
+//Marcus Wong
+//11/3/17
+//2nd Period
 import java.util.Arrays;
 
 public class ArrayLab3 {
@@ -12,32 +15,59 @@ public class ArrayLab3 {
 		int removeIdx = 5;
 		int[] removeArr = remove(a2, removeIdx);
 		
+		int sumOfEvens = sumEven(appendArr);
 		
+		rotateRight(a1);
 		
+		System.out.println(Arrays.toString(sumArr));
+		System.out.println(Arrays.toString(appendArr));
+		System.out.println(Arrays.toString(removeArr));
+		System.out.println(sumOfEvens);
+		System.out.println(Arrays.toString(a1));
 	}
 	public static int[] sum(int[] arr1, int[] arr2) {
 		int[] arr3 = new int[arr1.length];
-		for(int i = 0; i <= arr1.length; i++) {
+		for(int i = 0; i < arr1.length; i++) {
 			arr3[i] = arr1[i] + arr2[i];
 		}
 		return arr3;
 	}
 	public static int[] appened(int[] arr, int num) {
 		int[] arr2 = new int[arr.length + 1];
-		for(int i = 0; i <= arr.length; i++) {
+		for(int i = 0; i < arr.length; i++) {
 			arr2[i] = arr[i];
 		}
-		arr2[arr2.length] = num;
+		arr2[arr2.length-1] = num;
 		return arr2;
 	}
 	public static int[] remove(int[] arr, int idx) {
 		int[] returnArr = new int[arr.length - 1];
-		for(int i = 0; i <= (idx - 1); i++) {
-			returnArr[i] = arr[i];
-		}
-		for(int i = (idx + 1); i <= returnArr.length; i++) {
-			returnArr[i] = arr[i];
+		int counter = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (i != idx) {
+				returnArr[counter] = arr[i];
+				counter++;
+			}
 		}
 		return returnArr;
 	}
+	public static int sumEven(int[] arr) {
+		int sum = 0;
+		for(int i = 0; i < arr.length; i += 2) {
+			sum += arr[i];
+		}
+		return sum;
+	}
+	public static void rotateRight(int [] arr) {
+		int[] rotated = new int[arr.length];
+		int last = arr[arr.length-1];
+		for(int i = rotated.length - 1; i >= 1; i--) {
+			rotated[i] = arr[i-1];
+		}
+		rotated[0] = last;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = rotated[i];
+		}
+	}
+		
 }
