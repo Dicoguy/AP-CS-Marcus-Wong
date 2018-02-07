e175758f2438a473bcb3be2addbca6af0b7dc632
Dicoguy <31106989+Dicoguy@users.noreply.github.com>
2018-02-06 21:06:10 -0800

    completed client code for hobbit

--- TheHobbitRevised/src/ThereAndBackAgain.java
+++ TheHobbitRevised/src/ThereAndBackAgain.java
@@ -36,19 +36,16 @@ public class ThereAndBackAgain
 				               "Oin", "Gloin", "Bifur", "Bofur", "Bombur", "Thorin"};
 
 		// Make a new ArrayList to hold a 2nd party of Travelers called party2:
-		
+		ArrayList<Traveler> party2 = new ArrayList<Traveler>();
 		
 		// Call the createParty method and pass it party2 and the dwarfNames array.
-		
+		createParty(party2, dwarfNames);
 		
 		// Finally, call the allTravel method passing it party2 and 100 (representing
 		// the 100 miles that party2 has traveled together.  
-		
+		allTravel(party2, 100);
 
 
-		
-		
-		
 		
 		
 	}
@@ -58,9 +55,13 @@ public class ThereAndBackAgain
 	// dwarf names. This method will always add a new Hobbit named "Bilbo" and a      
 	// new Wizard named "Gandalf" whose color is "Grey" to the ArrayList.
 	// Then it uses a loop to add all the dwarves from the String array to the party.
-	public static void createParty(ArrayList<Traveler> party, String[] dwarfNames)
-	{	
-	
+	public static void createParty(ArrayList<Traveler> party, String[] dwarfNames){	
+		party.add(new Hobbit("Bilbo"));
+		party.add(new Wizard("Gandalf", "Grey"));
+		
+		for(String name: dwarfNames) {
+			party.add(new Dwarf(name));
+		}
 	}
 	
 	// The allTravel method accepts an ArrayList of Travelers and an integer number 
@@ -71,8 +72,12 @@ public class ThereAndBackAgain
 	//     Gandalf the grey has traveled 300 miles
 	//     fili has traveled 100 miles
 	//     kili has traveled 100 miles
-	public static String allTravel(ArrayList<Traveler> party, int miles)
-	{
-		return "";
+	public static String allTravel(ArrayList<Traveler> party, int miles){
+		String travels = "";
+		for(Traveler trav: party) {
+			trav.travel(miles);
+			travels += trav.getName() + "has traveled" + trav.getDistanceTraveled();
+		}
+		return travels;
 	}
 }
