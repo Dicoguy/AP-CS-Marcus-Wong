09ed3654261433ac299e4755e7b449a7bd85e83c
Dicoguy <31106989+Dicoguy@users.noreply.github.com>
2018-02-25 22:06:51 -0800

    sphere
    sdas

--- Prisms/Sphere.java
+++ Prisms/Sphere.java
@@ -1,4 +1,15 @@
 
-public class Sphere {
+public class Sphere extends ThreeDShape {
+	private double radius;
+	public Sphere(double radius) {
+		this.radius = radius;
+	}
 
+	public double calcAreaOfBase() {
+		return 4 * Math.PI * radius * radius;
+	}
+	
+	public  double calcPerimeter() {
+		return (4/3) * Math.PI * radius * radius * radius;
+	}
 }
