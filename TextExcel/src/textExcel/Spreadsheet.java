5fd98e1a2333758e639244d5d844eeb352763226
Dicoguy <sigore11@yahoo.com>
2018-03-12 09:46:13 -0700

    some checkpointn 1

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -1,29 +1,30 @@
+
+
 package textExcel;
 
 // Update this file with your own code.
 
-public class Spreadsheet implements Grid
-{
+public class Spreadsheet implements Grid{
 
+	private int rows;
+	private int cols;
+	
+	public Spreadsheet() {
+		int[][] spreadsheet = new int[rows][cols];
+	}
 	@Override
-	public String processCommand(String command)
-	{
-		// TODO Auto-generated method stub
+	public String processCommand(String command){
 		return null;
 	}
 
 	@Override
-	public int getRows()
-	{
-		// TODO Auto-generated method stub
-		return 0;
+	public int getRows(){
+		return rows;
 	}
 
 	@Override
-	public int getCols()
-	{
-		// TODO Auto-generated method stub
-		return 0;
+	public int getCols(){
+		return cols;
 	}
 
 	@Override
