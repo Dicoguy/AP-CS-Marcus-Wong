76562c623899dbb38930a016c02a8b4ca1ba3bfd
Dicoguy <sigore11@yahoo.com>
2018-03-20 11:45:32 -0700

    some checkpoint 2

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -5,27 +5,32 @@ package textExcel;
 // Update this file with your own code.
 
 public class Spreadsheet implements Grid{
-	
 
-	EmptyCell[][] spreadsheet = new EmptyCell[12][20];
+	Cell[][] spreadsheet = new Cell[12][20];
 	public Spreadsheet() {
-		
 		for(int row = 0; row < spreadsheet.length; row++) {
 			for(int col = 0; col < spreadsheet[row].length; col++){
 				spreadsheet[row][col] = new EmptyCell(); 
 			}
 		}
 	}
+	
 	@Override
 	public String processCommand(String command){
+		SpreadsheetLocation location = new SpreadsheetLocation(command);
 		if(command.contains("=")) {
 			
 		}else if(command.contains("clear") && (command.length() == 8) || (command.length() == 9) ) {
 			
 		}else if(command.contains("clear") && (command.length() == 5)) {
+			for(int row = 0; row < spreadsheet.length; row++) {
+				for(int col = 0; col < spreadsheet[row].length; col++){
+					spreadsheet[row][col] = new EmptyCell(); 
+				}
+			}
 			
 		}else if(command.contains("=")) {
-			
+			spreadsheet[getRow()][getCol]
 		}else {
 			
 		}
