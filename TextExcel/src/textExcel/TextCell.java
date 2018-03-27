e8df65aa2862cd17fc9bc35faf1562e3bdbbe838
Dicoguy <sigore11@yahoo.com>
2018-03-27 11:45:33 -0700

    attempt at quotes

--- TextExcel/src/textExcel/TextCell.java
+++ TextExcel/src/textExcel/TextCell.java
@@ -4,16 +4,29 @@ public class TextCell implements Cell{
 	String cellString;
 	
 	@Override
+	
 	public String abbreviatedCellText() {
 		String notCellString = cellString;
-		if(cellString.length() > 10) {
-			return cellString.substring(0,10);
-		}else {
-			for(int i = cellString.length(); i < 10; i++) {
-				notCellString += " ";
+		if(notCellString.contains("\"")) {
+			String noQuotes = notCellString.substring(1, notCellString.length() - 1);
+			
+			if(noQuotes.length() > 10) {
+				return noQuotes.substring(0,10);
+			}else {
+				for(int i = cellString.length(); i < 10; i++) {
+					noQuotes += " ";
+					return noQuotes;
+				}
 			}
-			//System.out.println("NotcellString " + notCellString);
-			return notCellString;
+		}else {
+			if(cellString.length() > 10) {
+				return cellString.substring(0,10);
+			}else {
+				for(int i = cellString.length(); i < 10; i++) {
+					notCellString += " ";
+				}
+				//System.out.println("NotcellString " + notCellString);
+				return notCellString;
 		}
 	}
 
