6c5b9e95e24078ab668f9844f6c0319512a0e446
Dicoguy <31106989+Dicoguy@users.noreply.github.com>
2018-03-27 22:09:48 -0700

    Abbreviated Cell text hopefully handles quotes

--- TextExcel/src/textExcel/TextCell.java
+++ TextExcel/src/textExcel/TextCell.java
@@ -4,30 +4,33 @@ public class TextCell implements Cell{
 	String cellString;
 	
 	@Override
-	
 	public String abbreviatedCellText() {
-		String notCellString = cellString;
-		if(notCellString.contains("\"")) {
-			String noQuotes = notCellString.substring(1, notCellString.length() - 1);
-			
-			if(noQuotes.length() > 10) {
-				return noQuotes.substring(0,10);
-			}else {
-				for(int i = cellString.length(); i < 10; i++) {
-					noQuotes += " ";
-					return noQuotes;
+		String cellStringTwo = cellString;
+		String returnString = "";
+		if(cellStringTwo.length() > 10) {
+			if(cellStringTwo.contains("\"")) {
+				String noQuotes = cellStringTwo.substring(1, cellStringTwo.length() - 1);
+				if(noQuotes.length() > 10) {
+					for(int i = 0; i < 10; i++) {
+						noQuotes += " ";
+						returnString += noQuotes.charAt(i);
+					}
+				}else {
+					returnString = noQuotes;
 				}
 			}
 		}else {
-			if(cellString.length() > 10) {
-				return cellString.substring(0,10);
-			}else {
-				for(int i = cellString.length(); i < 10; i++) {
-					notCellString += " ";
-				}
-				//System.out.println("NotcellString " + notCellString);
-				return notCellString;
+			if(cellStringTwo.contains("\"")) {
+				returnString = cellStringTwo.substring(1,cellStringTwo.length()-1);
+			}else{
+				returnString = cellStringTwo;
+			}
+	
+			for(int i = returnString.length(); i < 10; i++) {
+				returnString += " ";
+			}
 		}
+		return returnString;
 	}
 
 	@Override
