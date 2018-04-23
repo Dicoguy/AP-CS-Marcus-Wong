8ae20719e452ec6d4e3fa36fce176701128448f1
Dicoguy <31106989+Dicoguy@users.noreply.github.com>
2018-04-22 22:16:42 -0700

    some chkpt 4

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -1,5 +1,5 @@
 package textExcel;
-
+import java.util.ArrayList;
 public class FormulaCell extends RealCell{
 
 	public FormulaCell(String input) {
@@ -9,11 +9,37 @@ public class FormulaCell extends RealCell{
 
 	@Override
 	public String abbreviatedCellText() {
-		return null;
+		if((getDoubleValue() + "").length() > 10) {
+			return (getDoubleValue() + "").substring(0,10);
+		}else {
+			String spaces = "";
+			for(int i = 0; i < 10 - (getDoubleValue() + "").length(); i++) {
+				 spaces += " ";
+			}
+			return (getDoubleValue()) + spaces;
+		}
 	}
 	
 	public double getDoubleValue() {
+		String[] splitFormula = (numberString.substring(2,numberString.length()-2)).split(" ");
+		//takes an assignment such as ( 5 - 6 ) and turns it into an array of 5, "-" , 6
+		ArrayList<Double> doubles = new ArrayList<Double>();
+		ArrayList<String> strings = new ArrayList<String>();
 		
+		
+	}
+	public double operation(double numOne, String operand, double numTwo) {
+		double result = 0;
+		if(operand.equals("-")) {
+			result = numOne - numTwo; 
+		}else if(operand.equals("+")) {
+			result = numOne + numTwo;
+		}else if(operand.equals("*")) {
+			result = numOne * numTwo;
+		}else if(operand.equals("/")) {
+			result = numOne / numTwo;
+		}
+		return result;
 	}
 
 	@Override
